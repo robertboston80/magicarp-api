5eeccaa6cba6e26624546ed270a98bcef04cf32e
Darius Drachenfels Wiatrak <dariusz.wiatrak@gmail.com>
2018-04-30 16:26:00 +0100

    Add settings that are going to be in use by test suite.

---
+++ magicarp/settings/test.py
@@ -0,0 +1,7 @@
+# pylint: disable=wildcard-import, unused-wildcard-import
+from .base import *  # NOQA
+# pylint: enable=wildcard-import, unused-wildcard-import
+
+ENV = ENV_TEST  # NOQA
+
+DEBUG = True
