0cc3ba3e79d925aa78be147a87bd713e72d28912
Darius Drachenfels Viatrak <dariusz.wiatrak@essenceglobal.com>
2018-08-20 18:05:03 +0100

    Script start-app will give stack trace in case of fatal error on settings.

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-HOST=$(python -c 'from simple_settings import settings; print(settings.FLASK_SERVER_HOST)')
-PORT=$(python -c 'from simple_settings import settings; print(settings.FLASK_SERVER_PORT)')
+set -e
+
+# following line will fail with full stack trace if there is mistake in settings
+APP_NAME=$(python3 -c 'from magicarp.settings.base import APP_NAME; print(APP_NAME)')
+
+HOST=$(python3 -c 'from simple_settings import settings; print(settings.FLASK_SERVER_HOST)')
+PORT=$(python3 -c 'from simple_settings import settings; print(settings.FLASK_SERVER_PORT)')
 
 export FLASK_APP=magicarp.server_factory:create_app
 export DEBUG=$(python -c 'from simple_settings import settings; print(settings.DEBUG)')
